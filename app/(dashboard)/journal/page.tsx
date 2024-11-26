75f75e755513773a6e91c41272cc0f99d2a2006e
rayjay8 <jintarehaan@gmail.com>
2024-11-26 21:55:00 +0530

    dashboard setup

---
+++ app/(dashboard)/journal/page.tsx
@@ -0,0 +1,35 @@
+import NewEntryCard from '@/components/NewEntryCard'
+import EntryCard from '@/components/EntryCard'
+import { getUserByClerkID } from '@/util/auth'
+import { prisma } from '@/util/db'
+
+const getEntries = async () => {
+  const user = await getUserByClerkID()
+  const entries = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
+  return entries
+}
+
+const JournalPage = async () => {
+  const entries = await getEntries()
+  return (
+    <div className="p-10 bg-zinc-400/10 h-full">
+      <h2 className="text-3xl mb-8">Journal</h2>
+      <div className="grid grid-cols-3 gap-4">
+        <NewEntryCard />
+        {entries.map((entry) => (
+          <EntryCard key={entry.id} entry={entry} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default JournalPage
